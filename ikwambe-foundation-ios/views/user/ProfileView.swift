929de580a53c60601349926b055dcbe86ea4ba6b
Luuk Kenselaar <luuk@lkenselaar.dev>
2021-11-11 20:05:11 +0100

    Finished the basic profile page

---
+++ ikwambe-foundation-ios/views/user/ProfileView.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileView.swift
+//  ikwambe-foundation-ios
+//
+//  Created by Luuk Kenselaar on 11/11/2021.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var value = true
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text("Together we can help Ikwambe")
+                .font(.title3)
+            
+            HStack {
+                NavigationLink(destination: LoginView()) {
+                    Text("Login")
+                }
+                
+                NavigationLink(destination: SignupView()) {
+                    Text("Signup")
+                }
+            }
+            
+            Text("Updates")
+                .font(.title)
+            Toggle("Keep me up-to-date ", isOn: $value)
+            
+            Text("My donations")
+                .font(.title)
+            Text("Please login/signup to keep track of your donations")
+            
+            Spacer()
+        
+        }.navigationTitle("Profile")
+            .padding(.horizontal, 15)
+            .padding(.top, 15)
+        
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
