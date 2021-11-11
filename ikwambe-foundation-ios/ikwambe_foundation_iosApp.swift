9b78e54e5546589bcc361320fac8cbf9738fb3d6
Luuk Kenselaar <luuk@lkenselaar.dev>
2021-11-11 11:50:33 +0100

    First files

---
+++ ikwambe-foundation-ios/ikwambe_foundation_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikwambe_foundation_iosApp.swift
+//  ikwambe-foundation-ios
+//
+//  Created by Luuk Kenselaar on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ikwambe_foundation_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
