d46a1aeea5a7f31719dcd8bb6d134fab2fb97c9e
Luuk Kenselaar <luuk2014@hotmail.com>
2021-11-12 23:47:49 +0100

    Login and signup functionality (#2)
    * Basic login/signup request and response models
    
    * WIP: Login call
    
    * Finished basic login and signup

---
+++ ikwambe-foundation-ios/model/SignupRequest.swift
@@ -0,0 +1,16 @@
+//
+//  SignupRequest.swift
+//  ikwambe-foundation-ios
+//
+//  Created by Luuk Kenselaar on 12/11/2021.
+//
+
+import Foundation
+
+struct SignupRequest: Encodable {
+    let firstName: String
+    let lastName: String
+    let email: String
+    let password: String
+    let subscription: Bool
+}
--- ikwambe-foundation-ios/service/IkwambeAPI.swift
+++ ikwambe-foundation-ios/service/IkwambeAPI.swift
@@ -9,7 +9,7 @@ import Foundation
 import Alamofire
 import KeychainAccess
 
-class IkwambeAPI : ObservableObject {
+class IkwambeAPI: ObservableObject {
     static let shared = IkwambeAPI()
 
     private let keychain = Keychain()
@@ -36,12 +36,84 @@ class IkwambeAPI : ObservableObject {
         isAuthenticated = accessToken != nil
     }
     
-    func login(email: String, password: String) {
+    func login(email: String, password: String, completionHandler:
+    @escaping (Bool) -> ()) {
+        let headers: HTTPHeaders = [
+            .contentType("application/json")
+        ]
         
+        let data = LoginRequest (
+            email: email,
+            password: password
+        )
+                
+        AF.request("https://ikwambefoundation.azurewebsites.net/api/Login", method: .post, parameters: data, encoder: JSONParameterEncoder.default, headers: headers).response { response in
+            switch response.result {
+            case .success(let data):
+                if (response.response?.statusCode == 200) {
+                    do {
+                        let result = try JSONDecoder().decode(LoginResponse.self, from: data!)
+                        
+                        if (result.accessToken != "") {
+                            DispatchQueue.main.async {
+                                print(result.accessToken)
+                                self.accessToken = result.accessToken
+                            }
+                            completionHandler(true)
+                        } else {
+                            completionHandler(false)
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                } else {
+                    completionHandler(false)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completionHandler(false)
+            }
+        }
     }
     
-    func signup(fullname: String, email: String, password: String) {
+    func signup(firstName: String, lastName: String, email: String, password: String, completionHandler:
+    @escaping (Bool) -> ()) {
+        let headers: HTTPHeaders = [
+            .contentType("application/json")
+        ]
         
+        let data = SignupRequest(
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            subscription: true
+        )
+                
+        AF.request("https://ikwambefoundation.azurewebsites.net/api/users", method: .post, parameters: data, encoder: JSONParameterEncoder.default, headers: headers).response { response in
+            switch response.result {
+            case .success(let data):
+                print(response.response?.statusCode)
+                if (response.response?.statusCode == 200) {
+                    do {
+                        let result = try JSONDecoder().decode(SignupResponse.self, from: data!)
+                        
+                        if (result.userId != "") {
+                            completionHandler(true)
+                        } else {
+                            completionHandler(false)
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                } else {
+                    completionHandler(false)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completionHandler(false)
+            }
+        }.resume()
     }
     
     func logout() {
