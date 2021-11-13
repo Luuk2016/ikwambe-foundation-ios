9d01bc72087a5ac21747638d7722743ee0607dd2
Luuk Kenselaar <luuk@lkenselaar.dev>
2021-11-13 01:21:21 +0100

    Create required models + organized other models

---
+++ ikwambe-foundation-ios/model/donation/PayPalTransactionResponse.swift
@@ -0,0 +1,15 @@
+//
+//  PayPalTransactionResponse.swift
+//  ikwambe-foundation-ios
+//
+//  Created by Luuk Kenselaar on 13/11/2021.
+//
+
+import Foundation
+
+struct PayPalTransactionResponse: Codable {
+    let message: String
+    let link: String
+    let orderId: String
+    let status: Double
+}
