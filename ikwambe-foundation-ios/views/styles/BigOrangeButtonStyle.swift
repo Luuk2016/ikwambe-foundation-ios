65cb406fe1d83c13c2b03495796600a3c8bfd0a1
Luuk Kenselaar <luuk@lkenselaar.dev>
2021-12-06 17:56:31 +0100

    Button styles

--- ikwambe-foundation-ios/views/styles/BigOrangeButtonStyle.swift
+++ ikwambe-foundation-ios/views/styles/BigOrangeButtonStyle.swift
@@ -1,5 +1,5 @@
 //
-//  BigBlueButton.swift
+//  BigOrangeButton.swift.swift
 //  ikwambe-foundation-ios
 //
 //  Created by Luuk Kenselaar on 04/12/2021.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct BigBlueButton: ButtonStyle {
+struct BigOrangeButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.system(size: 24))
             .foregroundColor(.white)
-            .frame(width: 270, height: 57, alignment: .center)
+            .frame(width: 243, height: 51, alignment: .center)
             .padding(5)
-            .background(configuration.isPressed ? Color(red: 60 / 255, green: 148 / 255, blue: 242 / 255) : Color(red: 64 / 255, green: 156 / 255, blue: 255 / 255))
+            .background(configuration.isPressed ? Color(red: 242 / 255, green: 170 / 255, blue: 61 / 255) : Color(red: 255 / 255, green: 179 / 255, blue: 64 / 255))
             .cornerRadius(10)
     }
 }
--- ikwambe-foundation-ios/views/styles/SmallBlueButtonStyle.swift
+++ ikwambe-foundation-ios/views/styles/SmallBlueButtonStyle.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SmallBlueButton: ButtonStyle {
+struct SmallBlueButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.system(size: 18))
             .foregroundColor(.white)
-            .frame(width: 133, height: 47, alignment: .center)
+            .frame(width: 120, height: 42, alignment: .center)
             .padding(5)
             .background(configuration.isPressed ? Color(red: 60 / 255, green: 148 / 255, blue: 242 / 255) : Color(red: 64 / 255, green: 156 / 255, blue: 255 / 255))
             .cornerRadius(10)
